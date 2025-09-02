c8e81de76ebdbda1e3721727f87fe5f11ed5437f
Luanha2003 <Luanha2003@users.noreply.github.com>
2025-09-02 19:49:46 +0700

    update các trangtrang

--- obj/Debug/net9.0/web_cv.AssemblyInfo.cs
+++ obj/Debug/net9.0/web_cv.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web_cv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f725f08cd3f4e5eaa2e6581095c9076ec8449e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cbc9450d47ad2f0ae35bcd856c429848176ddf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("web_cv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web_cv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
