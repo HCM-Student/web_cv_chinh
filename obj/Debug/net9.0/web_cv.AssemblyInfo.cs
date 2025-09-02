fc4db2afde4fe6f3d2694d1f25464ad976ac6a53
Luanha2003 <Luanha2003@users.noreply.github.com>
2025-09-02 22:18:29 +0700

    layout_ admin

--- obj/Debug/net9.0/web_cv.AssemblyInfo.cs
+++ obj/Debug/net9.0/web_cv.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web_cv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8e81de76ebdbda1e3721727f87fe5f11ed5437f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2823670874ebc401bb5f21163c2dfb22e1a83d7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("web_cv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web_cv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
