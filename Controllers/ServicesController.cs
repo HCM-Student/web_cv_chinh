d227afc107c9279ecf015ed87f55330cfc048bfa
Luanha2003 <Luanha2003@users.noreply.github.com>
2025-09-03 23:52:17 +0700

    thêm các trang khác

---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace web_cv.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SmartCity3D()
+        {
+            return View();
+        }
+
+        public IActionResult LidarAndModeling()
+        {
+            return View();
+        }
+
+        public IActionResult DigitalTourism()
+        {
+            return View();
+        }
+
+        public IActionResult SurveyServices()
+        {
+            return View();
+        }
+
+        public IActionResult OtherServices()
+        {
+            return View();
+        }
+
+        public IActionResult GIS()
+        {
+            return View();
+        }
+    }
+}
