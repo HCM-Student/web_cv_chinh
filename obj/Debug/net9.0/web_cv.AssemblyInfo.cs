adb8d31d4554556da2735cdf470e2baf5760d2b4
Luanha2003 <Luanha2003@users.noreply.github.com>
2025-09-12 10:21:22 +0700

    fix header

--- obj/Debug/net9.0/web_cv.AssemblyInfo.cs
+++ obj/Debug/net9.0/web_cv.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web_cv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14967eae74d85d06675f6b69695843676bbd0a72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df2669cf64cd2858b1b115b178db94a26eb7a9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("web_cv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web_cv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
